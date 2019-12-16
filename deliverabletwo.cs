729ceab214c846b8a248df66feb4d520c35bf867
Jonathan Parnell II <JonathanParnell2@Jonathans-MacBook-Air.local>
2019-12-16 10:19:47 -0500

    first commit for delieverable one

---
+++ deliverabletwo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var input = "";
+        string message = null;
+        int checksum = 0;
+
+        Console.WriteLine("What is your message?");
+        input = Console.ReadLine();
+        input = input.ToUpper();
+
+        foreach (char c in input)
+        {
+            if (message != null)
+
+            {
+
+                message += "-";
+
+            }
+
+            else
+
+            {
+
+
+                message = "";
+
+            }
+
+            message += c - 64;
+            checksum += c;
+        }
+
+        Console.WriteLine(message);
+        Console.WriteLine(checksum);
+
+    }
+}
